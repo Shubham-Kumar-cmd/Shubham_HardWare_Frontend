ac376eb77aa509ee2d5c2e98ab77dcb15e88b3da
Shubham-Kumar-cmd <kumar53shubham@gmail.com>
2023-10-30 23:07:54 +0530

    added pagination in ViewProducts

--- src/pages/admin/ViewProducts.jsx
+++ src/pages/admin/ViewProducts.jsx
@@ -5,17 +5,18 @@ import { Card, Col, Container, Form, Pagination, Row, Table } from 'react-bootst
 import { getAllProducts } from '../../services/ProductService'
 import { toast } from 'react-toastify'
 import SingleProductView from '../../components/admin/SingleProductView'
+import { PRODUCT_PAGE_SIZE } from '../../services/HelperService'
 
 const ViewProducts = () => {
 
   const [products, setProducts] = useState(undefined)
 
   useEffect(() => {
-    allProducts(0,10,'addedDate','desc')
+    getProducts(0, PRODUCT_PAGE_SIZE, 'addedDate', 'desc')
   }, [])
 
 
-  const allProducts = (pageNumber, pageSize, sortBy, sortDir) => {
+  const getProducts = (pageNumber, pageSize, sortBy, sortDir) => {
     //get all product api call  
     getAllProducts(pageNumber, pageSize, sortBy, sortDir)
       .then(data => {
@@ -68,19 +69,50 @@ const ViewProducts = () => {
             <tbody>
               {
                 products.content.map((product, index) => (
-                  <SingleProductView key={index} product={product} index={index}/>
+                  <SingleProductView key={index} product={product} index={index} />
                 ))
               }
             </tbody>
           </Table>
           <Container className='d-flex justify-content-end mt-3'>
             <Pagination className='ms-auto'>
-              <Pagination.Prev />
-              <Pagination.Item>1</Pagination.Item>
-              <Pagination.Item>2</Pagination.Item>
-              <Pagination.Item>3</Pagination.Item>
-              <Pagination.Item>4</Pagination.Item>
-              <Pagination.Next />
+              {/* 0 to totalPages-1 */}
+
+              {/* first page */}
+              <Pagination.First onClick={event => {
+                getProducts(0, PRODUCT_PAGE_SIZE, 'addedDate', 'desc')
+              }} />
+
+              {/* prev page */}
+              <Pagination.Prev onClick={event => {
+                if ((products.pageNumber - 1) < 0) {
+                  return;
+                }
+                getProducts(products.pageNumber - 1, PRODUCT_PAGE_SIZE, 'addedDate', 'desc')
+              }} />
+
+              {
+                [...Array(products.totalPages)].map((ob, i) => i).map(item => (
+                  (products.pageNumber === item ? <Pagination.Item key={item} active>{item + 1}</Pagination.Item> : <Pagination.Item onClick={event => {
+                    getProducts(item, PRODUCT_PAGE_SIZE, 'addedDate', 'desc')
+                  }} key={item}>{item + 1}</Pagination.Item>)
+                ))
+              }
+              
+              <Pagination.Ellipsis/>
+
+              {/* next page */}
+              <Pagination.Next onClick={event => {
+                if (products.lastPage) {
+                  return;
+                }
+                getProducts(products.pageNumber + 1, PRODUCT_PAGE_SIZE, 'addedDate', 'desc')
+              }} />
+
+              {/* last page */}
+              <Pagination.Last onClick={event => {
+                getProducts(products.totalPages - 1, PRODUCT_PAGE_SIZE, 'addedDate', 'desc')
+              }} />
             </Pagination>
           </Container>
         </Card.Body>
--- src/services/HelperService.js
+++ src/services/HelperService.js
@@ -1,1 +1,3 @@
 export const BASE_URL=`http://localhost:8083`
+
+export const PRODUCT_PAGE_SIZE=10
